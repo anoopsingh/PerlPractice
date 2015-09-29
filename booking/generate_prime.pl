ebff8dcf9f9066d26c8ecb2b838dffe9d9c71f9b
anoopsingh <anoop1186@gmail.com>
2015-09-29 11:32:03 +0530

    booking folder removed

--- booking/generate_prime.pl
+++
@@ -1,5 +0,0 @@
-sub sieve {
-grep{@_[map$a*$_,$_..@_/($a=$_)]=0if$_[$_]>1}@_=0..pop
-}
-
-print join " ", sieve(1000000);
--- booking/test.pl
+++
@@ -1,8 +0,0 @@
-$num = 'szfsdfgdsgdsfsf';
-print "-------------$num\n";
-while ( length($num) > 0 ) {
-    $k = substr($num, 0, 1);
-    substr($num, 0, 1) = '';
-    print "-------------$k\n";
-    print "-------------$num\n";
-}
